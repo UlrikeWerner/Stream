6a9ff302f786ac4682cf3e0cf6815362c3b705de
UlrikeWerner <ulrike_werner@web.de>
2023-09-07 12:29:23 +0200

    correct naming

--- src/main/java/com/github/UlrikeWerner/Stream/Student.java
+++ src/main/java/com/github/UlrikeWerner/Stream/Student.java
@@ -5,21 +5,21 @@ import java.util.Objects;
 public class Student {
     private int id;
     private String name;
-    private int points;
+    private int postalCode;
     private int age;
 
     public Student(String studentData) {
         String[] student = studentData.split(",");
         this.id = Integer.parseInt(student[0]);
         this.name = student[1];
-        this.points = Integer.parseInt(student[2]);
+        this.postalCode = Integer.parseInt(student[2]);
         this.age = Integer.parseInt(student[3]);
     }
 
-    public Student(int id, String name, int points, int age) {
+    public Student(int id, String name, int postalCode, int age) {
         this.id = id;
         this.name = name;
-        this.points = points;
+        this.postalCode = postalCode;
         this.age = age;
     }
 
@@ -39,12 +39,12 @@ public class Student {
         this.name = name;
     }
 
-    public int getPoints() {
-        return points;
+    public int getPostalCode() {
+        return postalCode;
     }
 
-    public void setPoints(int points) {
-        this.points = points;
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
     }
 
     public int getAge() {
@@ -60,12 +60,12 @@ public class Student {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
-        return id == student.id && points == student.points && age == student.age && Objects.equals(name, student.name);
+        return id == student.id && postalCode == student.postalCode && age == student.age && Objects.equals(name, student.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, points, age);
+        return Objects.hash(id, name, postalCode, age);
     }
 
     @Override
@@ -73,7 +73,7 @@ public class Student {
         return "Student{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", points=" + points +
+                ", postalCode=" + postalCode +
                 ", age=" + age +
                 '}';
     }
